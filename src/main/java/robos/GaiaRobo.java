476dcd898f51b64473d2ed17cb480b75b0b0765d
ThalitaGusmao <thalitagusmaosilveira@gmail.com>
2022-03-22 21:38:10 -0300

    Criacao de classes e metodos

---
+++ src/main/java/robos/GaiaRobo.java
@@ -0,0 +1,15 @@
+package robos;
+
+public class GaiaRobo {
+     public void fazerLogin() {
+         System.out.println("Vou fazer login!");
+
+     }
+
+     public void lancarNota() {
+         System.out.println("Vou lançar nota!");
+
+     }
+
+
+}
